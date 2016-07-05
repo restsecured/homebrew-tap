3fa45b389797f4a4027b2d580f6c5aba1fce2f4b
stankiewicz <rrydziu@gmail.com>
2016-07-05 11:14:42 +0200

    update version

--- restsecured-copilot.rb
+++ restsecured-copilot.rb
@@ -4,9 +4,9 @@
 class RestsecuredCopilot < Formula
   desc "Rest Secured Copilot - Security Consultant at your service"
   homepage "https://www.restsecured.xyz"
-  url "https://github.com/stankiewicz/restsecured-copilot/archive/0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "cb32a9276627d12d6c5677315b5e52f4cf6f3ab5c02298bb3c1e726e215fc67f"
+  url "https://github.com/stankiewicz/restsecured-copilot/archive/0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "19589b020e7464c4c32d92ba5faa06fecf50dd15b318c81a23f55667c824968b"
 
   # depends_on "cmake" => :build
   depends_on :python
@@ -14,10 +14,13 @@ class RestsecuredCopilot < Formula
     url "https://pypi.python.org/packages/source/r/requests/requests-2.5.1.tar.gz"
     sha1 "f906c441be2f0e7a834cbf701a72788d3ac3d144"
   end
+  resource "transitions" do
+    url "https://github.com/tyarkoni/transitions/archive/0.4.0.tar.gz"
+    sha1 "065611458c52e58fd40cc35f57167972fc627035"
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    %w[requests].each do |r|
+    %w[requests transitions].each do |r|
       resource(r).stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
